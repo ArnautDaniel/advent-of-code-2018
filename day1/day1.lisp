103a71721e0d864f75bc475291597e3e7b76913b
Jack Lucas <silverbeard@protonmail.com>
2018-12-01 08:38:47 -0500

    day 1

---
+++ day1/day1.lisp
@@ -0,0 +1,27 @@
+(defun hash-fold (cur data found)
+  (let ((try-find (hash-iterate cur  data '() found)))
+    (if (numberp try-find)
+	try-find
+	(hash-fold  (car try-find) data (append  (cdr try-find) found)))))
+
+
+(defun hash-iterate (cur data finder found)
+  (cond
+    ((null data)
+     (cons cur (reverse finder)))
+    ((member (+ cur (car data)) found)
+     (car (member (+ cur (car data)) found)))
+    (t
+     (hash-iterate (+ cur (car data)) (cdr data)
+		   (cons (+ cur (car data)) finder) found))))
+
+(defun get-file (filename)
+  (with-open-file (stream filename)
+    (loop for line = (read-line stream nil)
+       while line
+       collect line)))
+
+(defun data-to-int (data)
+  (mapcar #'(lambda (x) (if (string= "" x)
+			    0
+			    (parse-integer x))) data))
