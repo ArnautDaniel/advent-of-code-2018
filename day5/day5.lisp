8b51af0f0d3978351b75365d516d7a24575aa943
Jack Lucas <silverbeard@protonmail.com>
2018-12-05 08:12:53 -0500

    slight cleanup

--- day5/day5.lisp
+++ day5/day5.lisp
@@ -16,47 +16,40 @@
       (list charl (char-downcase charl))
       (list charl (char-upcase charl))))
 
-(defun answer2-loop (data constraint acc)
+(defun char-case-pair? (a b)
+  (and
+   (or
+    (and (upper-case-p a) (lower-case-p b))
+    (and (lower-case-p a) (upper-case-p b)))
+   (char= (char-downcase a) (char-downcase b))))
+
+(defun answer-loop (data acc)
   (cond
     ((null data)
      acc)
     ((null (cdr data))
-     (append acc (list (car data))))
-    ((or
-      (and
-       (member (car data) constraint)
-       (member (second data) (remove (car data) constraint)))
-      (and
-       (member (second data) constraint)
-       (member (car data) (remove (second data) constraint))))
-     (answer2-loop (cdr (cdr data)) constraint acc))
+     (append (reverse acc) (list (car data))))
+    ((char-case-pair? (car data) (cadr data))
+     (answer-loop (cdr (cdr data)) acc))
     (t
-     (answer2-loop (cdr data) constraint (cons (car data) acc)))))
+     (answer-loop (cdr data) (cons (car data) acc)))))
 
-  (defun answer-loop (data acc)
-    (cond
-      ((null data)
-       acc)
-      ((null (cdr data))
-       (append (reverse acc) (list (car data))))
-      ((and (or
-	     (and (upper-case-p (car data)) (lower-case-p (second data)))
-	     (and (lower-case-p (car data)) (upper-case-p (second data))))
-	    (char= (char-downcase (car data)) (char-downcase (second data))))
-       (answer-loop (cdr (cdr data)) acc))
-      (t
-       (answer-loop (cdr data) (cons (car data) acc)))))
+(defun answer (data)
+  (let ((len (length data)))
+    (let ((res (answer-loop data '())))
+      (if (= (length res) len)
+	  res
+	  (answer res)))))
 
-  (defun answer (data)
-    (let ((len (length data)))
-      (let ((res (answer-loop data '())))
-	(if (= (length res) len)
-	    res
-	    (answer res)))))
-  
-  (defun answer2 (data constraint)
-    (let ((len (length data)))
-      (let ((res (answer2-loop data constraint '())))
-	(if (= (length res) len)
-	    res
-	    (answer2 res constraint)))))
+(defun answer2 (data x)
+  (let ((constraint (make-constraint x)))
+    (answer
+     (remove (car constraint)
+	     (remove (cadr constraint)
+		     data)))))
+
+(defun answer2-map (data)
+  (let ((unique (remove-duplicates (mapcar #'char-downcase data))))
+    (car (sort (mapcar #'(lambda (x) (length (answer2 data x)))
+		  unique)
+	  #'<))))
